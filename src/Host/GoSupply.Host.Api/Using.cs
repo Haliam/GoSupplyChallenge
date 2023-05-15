51e3b04c7ab30e50eeff0868039a03701632514d
Haliam <haliampf@gmail.com>
2023-05-15 12:40:22 +0100

    Add StudentsController

--- src/Host/GoSupply.Host.Api/Using.cs
+++ src/Host/GoSupply.Host.Api/Using.cs
@@ -1,1 +1,6 @@
 ﻿global using GoSupply.Infrastructure.Persistence.Database;
+global using GoSupply.Infrastructure.Persistence.Repositories;
+global using Microsoft.AspNetCore.Mvc;
+global using GoSupply.Application.AppServices;
+global using GoSupply.Application.Dtos;
+
