97032923ff1c3a7c74d99f97019c71f16d563610
Haliam <haliampf@gmail.com>
2023-05-17 13:16:48 +0100

    Add Repositories IUserRepository and mapping

--- src/Application/Using.cs
+++ src/Application/Using.cs
@@ -1,6 +1,11 @@
 ﻿global using AutoMapper;
 global using GoSupply.Application.Dtos;
+global using GoSupply.Domain.Entities;
+global using GoSupply.Infrastructure.Persistence.Repositories;
 global using System.ComponentModel.DataAnnotations;
 global using System.Text.Json.Serialization;
-global using GoSupply.Infrastructure.Persistence.Repositories;
-global using GoSupply.Domain.Entities;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
